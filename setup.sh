3cf4b058315e3e177c6809df1e32fb9a22b6b647
Kevin Tan Eu Jin <kejtan@majurutera.com>
2024-01-12 21:51:09 +0800

    more fixes to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ln -s $SCRIPT_PATH/.vimrc $HOME/.vimrc
 ln -s $SCRIPT_PATH/.tmux.conf $HOME/.tmux.conf 
-if [ -f $HOEM/.config/nvim/init.vim ]; then
-mkdir $HOME/.config/nvim 2>/dev/null
+if [ ! -f $HOME/.config/nvim/init.vim ]; then
+mkdir $HOME/.config/nvim 
 cat > $HOME/.config/nvim/init.vim <<EOF
 source ~/.vimrc
 EOF
